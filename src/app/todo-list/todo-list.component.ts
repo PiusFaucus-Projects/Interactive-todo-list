c9836c5d0f5dc2ba2fd277be947e9aab29e2b547
Vlad Iacob <vlad.iacob@hackajob.co>
2021-09-10 10:28:19 +0300

    Correct identation and a new identation file for the IDE.

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,38 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import * as uuid from 'uuid';
 
-import { TODOS } from '../todos';
+import {TODOS} from '../todos';
 
 @Component({
-    selector: 'app-todo-list',
-    templateUrl: './todo-list.component.html',
-    styleUrls: ['./todo-list.component.css']
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.css']
 })
 export class TodoListComponent implements OnInit {
-    todos = TODOS;
+  todos = TODOS;
 
-    constructor() {
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  createTodo(newTodoName) {
+    if (!newTodoName) {
+      return;
     }
 
-    ngOnInit() {
-    }
+    const todoId = uuid.v4();
+    this.todos.push({
+      id: todoId,
+      name: newTodoName
+    });
+  }
 
-    createTodo(newTodoName) {
-        if (!newTodoName) {
-            return;
-        }
-
-        const todoId = uuid.v4();
-        this.todos.push({
-            id: todoId,
-            name: newTodoName
-        });
+  deleteTodo(itemToDelete) {
+    const todoIdx = this.todos.findIndex((todo) => todo.id === itemToDelete.id);
+    if (todoIdx > -1) {
+      this.todos.splice(todoIdx, 1);
     }
-
-    deleteTodo(itemToDelete) {
-        const todoIdx = this.todos.findIndex((todo) => todo.id === itemToDelete.id);
-        if (todoIdx > -1) {
-            this.todos.splice(todoIdx, 1);
-        }
-    }
+  }
 }
