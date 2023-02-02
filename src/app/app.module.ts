d27ae566d72d8b05b0785fd40b1aefea853a1be0
candidate-171781 <candidate-171781@candidate.hackajob.co>
2023-02-02 09:36:42 +0000

    auto-commit-agent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-import {FormsModule}
+import {FormsModule} from '@'
 
 import {AppComponent} from './app.component';
 import {TodoListComponent} from './todo-list/todo-list.component';
